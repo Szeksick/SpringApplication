99bf8240d7bb6ac457a9818189ec879c932370c4
Szeksick <konradgugala@gmail.com>
2019-09-30 09:42:51 +0200

    user authentification to improve

--- src/main/java/codes/szeksick/springimagehostapp/model/AplicationUser.java
+++ src/main/java/codes/szeksick/springimagehostapp/model/AplicationUser.java
@@ -3,12 +3,14 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Collection;
 import java.util.Collections;
 
+@Entity
 public class AplicationUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
